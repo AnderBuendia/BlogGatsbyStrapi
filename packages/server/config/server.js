c3db372e3bc8dcc13f29ad51ab47d88322611d6b
Ander Buendia <ibysytho@gmail.com>
2020-11-16 20:52:51 +0100

    backend changes

---
+++ packages/server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8aa70c152238664ade9fc175085fb96b'),
+    },
+  },
+});
---
+++ packages/server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '76b6f150-a8e3-4cb1-a098-462837fc934b'
+};
